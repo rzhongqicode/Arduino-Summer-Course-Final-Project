11c61e18b60c5049bfac18ab104350ea2bf65b94
Zhongqi Ren（pc） <rzq23@mails.tsinghua.edu.cn>
2025-08-19 14:38:28 +0800

    添加了bitmap.h，esp的开机动画，ard的按键和蓝灯呼吸

--- task23/arduino/arduino.ino
+++ task23/arduino/arduino.ino
@@ -3,6 +3,8 @@
 const int button_1_pin = 8;
 const int button_2_pin = 2;
 const int red_pin = 9, green_pin = 10, blue_pin = 11;
+const int buzz_pin = 3;
+
 //标定当前的状态
 char cur_mode = 'W';
 //W : 欢迎界面
@@ -12,6 +14,8 @@ char cur_mode = 'W';
 //O : 示波器
 //M : 音乐
 //F : 闪光灯
+bool threshold = false;
+bool music_is_on = false;
 
 //标定Led状态的变量
 int LedState = 0;
@@ -19,6 +23,17 @@ int LedState = 0;
 //1：微蓝
 //2：微白
 //3：白
+
+//音乐播放有关量
+int cur_beat = 0;//标定当前第几个音节
+unsigned long prev_beatTime = 0;//标定上个音节的开始时间
+
+//蓝灯呼吸有关量
+unsigned long prev_breath_time = 0;
+int interval_bearth = 50;
+int step_breath = 10;
+int direction_breath = 1;
+int breath_value = 0;
 
 OneButton button_1(button_1_pin, true);
 OneButton button_2(button_2_pin, true);
@@ -32,6 +47,24 @@ void updateLed(){
     case 3: analogWrite(red_pin, 255); analogWrite(green_pin, 255); analogWrite(blue_pin, 255); break;
   }
 }
+//蓝灯呼吸功能
+void blueLed_breathe(){
+  if(millis() - prev_breath_time >= interval_bearth){
+    breath_value += direction_breath * step_breath;
+    prev_breath_time = millis();
+    if(breath_value >= 255){
+      direction_breath = -1;
+      breath_value = 255;
+    }
+    if(breath_value <= 0){
+      direction_breath = 1;
+      breath_value = 0;
+    }
+    analogWrite(blue_pin, breath_value);
+  }
+}
+
+
 //按键1单击
 void button1_singleClick(){
   if(cur_mode == 'F'){
@@ -40,15 +73,62 @@ void button1_singleClick(){
     updateLed();
   }
 }
+void button1_doubleClick(){
+  if(cur_mode == 'U'){//在主菜单
+    Serial.println("c"); //发送c (confirm)确定当前功能, 此处需要确定当前cur_mode？？？
+  }
+  if(cur_mode == 'F'){//在闪光灯
+    digitalWrite(red_pin, LOW);
+    digitalWrite(blue_pin, LOW);
+    digitalWrite(green_pin, LOW);
+  }
+}
+void button1_longpress(){
+  Serial.println("u"); //发送u, ui菜单，返回主菜单
+  cur_mode = 'U';
+}
+void button2_longpress()
+{
+  if(cur_mode == 'R'){//在测距模式
+    Serial.println("r");
+    threshold = !threshold;
+  }
+}
+void button2_singleClick(){
+   if(cur_mode == 'O'){//在示波器
+    Serial.println("c"); //发送c， change改变通道
+  }
+  if(cur_mode == 'M'){//在音乐
+    if(music_is_on){
+      noTone(buzz_pin);
+    }
+    else{
+      cur_beat = 0;
+      prev_beatTime = millis();
+    }
+    music_is_on = !music_is_on;
+  }
+}
 
 void setup() {
+  Serial.begin(9600);
   pinMode(red_pin, OUTPUT);
   pinMode(green_pin, OUTPUT);
   pinMode(blue_pin, OUTPUT);
+  pinMode(buzz_pin, OUTPUT);
   //绑定按键行为
+  //按键1单击
   button_1.attachClick(button1_singleClick);
-
+  //按键1双击
+  button_1.attachDoubleClick(button1_doubleClick);
+  //按键1长按
+  button_1.attachLongPressStop(button1_longpress);
+  //按键2长按
+  button_2.attachLongPressStop(button2_longpress);
+  //按键2单击
+  button_2.attachClick(button2_singleClick);
 }
+
 
 void loop() {
   button_1.tick();
--- task23/esp8266/esp8266.ino
+++ task23/esp8266/esp8266.ino
@@ -1,16 +1,45 @@
 #include <U8g2lib.h>
 #include <SoftwareSerial.h>
+#include "bitmap.h"
 #define SCL_PIN 5
 #define SDA_PIN 4
 #define RX_PIN 13
 #define TX_PIN 15
 #define ENCODER_A 12
 #define ENCODER_B 14
+//判断当前所处的状态的变量
+bool inMenu = false;
+bool inFunction = false;
+bool thresholdMode = false;
 
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, SCL_PIN, SDA_PIN, U8X8_PIN_NONE);
 SoftwareSerial arduinoSerial(RX_PIN, TX_PIN);
+
 void setup() {
-  
+  pinMode(ENCODER_A, INPUT_PULLUP);
+  pinMode(ENCODER_B, INPUT_PULLUP);
+  //配置内置LED
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH);
+  arduinoSerial.begin(9600);
+  //开机动画和欢迎界面
+  u8g2.begin();
+  u8g2.enableUTF8Print();
+  for (int i = -32; i < (128 - 32) / 2; i += 4) {
+    u8g2.clearBuffer();
+    u8g2.drawXBMP(i, (64 - 32) / 2, 32, 32, epd_bitmap_icon_cube_00);
+    u8g2.sendBuffer();
+    delay(20);
+  }
+  u8g2.clearBuffer();
+  u8g2.setFont(u8g2_font_ncenB14_tr);
+  u8g2.drawStr(20, 40, "Welcome!");
+  u8g2.sendBuffer();
+  delay(1500);
+  digitalWrite(LED_BUILTIN, HIGH);
+  u8g2.setDrawColor(1); 
+  u8g2.setBitmapMode(1); 
+  u8g2.setFontMode(1);
 }
 
 void loop() {
